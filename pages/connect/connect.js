9b3b2b49a553417cfd0cd2688039e1a3170de3b1
PandyYang <fry227662112@gmail.com>
2019-09-26 23:50:48 +0800

    蓝牙初始化与连接模块开发

---
+++ pages/connect/connect.js
@@ -0,0 +1,100 @@
+/** connble.js */
+
+Page({
+  data: {
+    deviceId: '',
+    name: '',
+    RSSI: '',
+    advData: '',
+    serviceId: '',
+    serviceList: [],
+    serviceUUID: '',
+    select: '',
+    characteristics: [],
+  },
+
+  onLoad: function (opt) {
+    var that = this;
+    console.log("onLoad");
+    console.log('deviceId=' + opt.deviceId);
+    console.log('name=' + opt.name);
+    console.log('RSSI=' + opt.RSSI);
+    console.log('advData=' + opt.advData);
+    //从scanble导航来的数据
+    that.setData({
+      RSSI: opt.RSSI,
+      name: opt.name,
+      deviceId: opt.deviceId,
+      advData: opt.advData,
+    });
+  },
+
+  bindConnect: function () {
+    var that = this;
+    //连接低功耗蓝牙设备 deviceId timeout success fail complete
+    wx.createBLEConnection({
+      deviceId: that.data.deviceId,
+      //create conn. success
+      success: function (res) {
+        console.log(res);
+        //获取蓝牙设备所有服务
+        wx.getBLEDeviceServices({
+          deviceId: that.data.deviceId,
+          success: function (res) {
+            console.log('device services:', res.services);
+            that.setData({
+              serviceList: res.services,
+            })
+            console.log('serviceList:', that.data.serviceList);
+          },
+        });
+      },
+      //连接失败
+      fail: function (res) {
+        wx.showToast({
+          title: '建立连接失败',
+          icon: 'loading',
+          duration: 2000
+        })
+      },
+    })
+
+  },
+
+  //res.services结构的uuid 蓝牙设备服务的uuid
+  selectUUID: function (e) {
+    var that = this;
+    var uuid = e.currentTarget.dataset.uuid;
+    console.log(uuid);
+    //show selected service uuid
+    that.setData({
+      serviceUUID: uuid,
+      select: 1,
+    })
+
+  },
+
+  getCharc: function () {
+    var that = this;
+    if(that.data.select == ''){
+      wx.showToast({
+        title: '请先选择service uuid',
+        icon: 'loading',
+        duration: 2000
+      })
+    }
+    //获取蓝牙设备某个服务中所有特征值 deviceId:蓝牙设备id,serviceId:蓝牙服务uuid
+    wx.getBLEDeviceCharacteristics({
+      deviceId: that.data.deviceId,
+      serviceId: that.data.serviceUUID,
+      success: function(res) {
+        console.log('getBLEDeviceCharacteristics:', res.characteristics)
+        that.setData({
+          characteristics: res.characteristics,
+        })
+      },
+    })
+
+  },
+
+})
---
+++ pages/search/search.js
@@ -0,0 +1,106 @@
+/** scanble.js */
+
+Page({
+  data: {
+    logs: [],
+    list: [],
+  },
+
+  onShow: function () {
+    console.log('onShow');
+    var that = this;
+    var dev = that.data.list; //设备列表
+    
+    /**
+     * 初始化蓝牙模块
+     * 参数列表:
+     *    success  function  非必填 接口调用成功的回调函数
+     *    fail     function  非必填 接口调用失败的回调函数
+     *    complete function  非必填 接口调用结束的回调函数(调用成功或失败都会返回)
+     */
+    wx.openBluetoothAdapter({
+      //打开蓝牙模块成功
+      success: function (res) {
+        console.log("openBluetoothAdapter: success");
+        console.log(res);
+        //开始搜寻附近的蓝牙外围设备
+        wx.startBluetoothDevicesDiscovery({
+          /**
+           * 要搜索的蓝牙设备主 service 的 uuid 列表。某些蓝牙设备会广播自己的主 service 的 uuid。
+           * 如果设置此参数，则只搜索广播包有对应 uuid 的主服务的蓝牙设备。建议主要通过该参数过滤掉周边不需要处理的其他蓝牙设备。
+           * 此外参数allowDuplicatesKey还可以设置是否允许重复上报同一设备;interval:上报设备的间隔,0表示立即上报.
+           */
+          services: [],
+          success: function (res) {
+            console.log("startBluetoothDevicesDiscovery: success");
+            console.log(res);
+            //监听寻找到新设备的事件
+            wx.onBluetoothDeviceFound(function (res) {
+              console.log('new device list has founded');
+              console.log(res);
+              //found one device only, reflesh device list each time
+              dev.push(res.devices[0]);
+              that.setData({
+                list: dev
+              });
+              console.log('that.setData: list');
+              console.log(that.data.list);
+            })
+          },
+        })
+      },
+
+      //打开蓝牙模块失败
+      fail: function (res) {
+        console.log("openBluetoothAdapter: fail");
+        console.log(res);
+        wx.showToast({
+          title: '搜索失败，请确认已经打开蓝牙',
+          icon: "loading",
+          duration: 3000
+        })
+      },
+    })
+
+  },
+
+  onUnload: function () {
+    //停止搜索蓝牙设备
+    wx.stopBluetoothDevicesDiscovery({
+      success: function (res) {
+        console.log("stopBluetoothDevicesDiscovery: success");
+      },
+    })
+    //关闭蓝牙模块
+    wx.closeBluetoothAdapter({
+      success: function (res) {
+        console.log("closeBluetoothAdapter: success");
+      },
+    })
+
+  },
+  
+  connectDevice: function (e) {
+    //停止搜索附近的蓝牙外围设备
+    wx.stopBluetoothDevicesDiscovery({
+      success: function (res) {
+        console.log(res)
+      }
+    })
+    //从scanble.wxml获取设备结构
+    var deviceid = e.currentTarget.dataset.deviceid;
+    var name = e.currentTarget.dataset.name;
+    var rssi = e.currentTarget.dataset.rssi;
+    var advdata = e.currentTarget.dataset.advdata;
+    console.log(deviceid);
+    console.log(name);
+    console.log(rssi);
+    console.log(advdata);
+    //导航到包含数据的新connble页
+    wx.navigateTo({
+      url: '../connect/connect?deviceId=' + deviceid + '&name=' + name + '&RSSI=' + rssi + '&advData=' + advdata,
+    })
+
+  },
+
+})
